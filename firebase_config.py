dd1493b03a960686fa58b077c98239d7acac78c6
Joshua Sevilla <jSevilla@mcm.edu.ph>
2024-05-29 16:05:25 +0800

    meow3

--- firebase_config.py
+++ firebase_config.py
@@ -7,5 +7,5 @@ SERVICE_ACCOUNT_KEY_PATH = '/serviceAccountKey.json'
 # Initialize the app with a service account, granting admin privileges
 cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://<your-database-name>.firebaseio.com'
+    'databaseURL': 'https://sync-24-default-rtdb.asia-southeast1.firebasedatabase.app'
 })
